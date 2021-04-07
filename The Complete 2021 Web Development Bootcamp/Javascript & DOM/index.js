5809a9cbb0578038ac6c613379fd0d39fc37462c
unknown <klinton.jp@gmail.com>
2021-04-07 13:05:17 -0600

    added emoji

--- The Complete 2021 Web Development Bootcamp/Javascript & DOM/index.js
+++ The Complete 2021 Web Development Bootcamp/Javascript & DOM/index.js
@@ -11,9 +11,9 @@ document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}
 document.querySelector(".img2").setAttribute("src", `images/dice${randomNumber2}.png`);
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent= "Player 1 Wins!";
+    document.querySelector("h1").textContent= "㊗️Player 1 Wins!";
 } else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").textContent= "Player 2 Wins!";
+    document.querySelector("h1").textContent= "Player 2 Wins!㊗️";
 } else {
     document.querySelector("h1").textContent= "No one wins, refresh again!";
 }

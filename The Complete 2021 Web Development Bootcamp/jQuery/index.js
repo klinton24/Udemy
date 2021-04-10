1d3752ffb32e176be3cef843d0c14cb3e1343e66
unknown <klinton.jp@gmail.com>
2021-04-10 08:30:35 -0600

    added class using jQuery

---
+++ The Complete 2021 Web Development Bootcamp/jQuery/index.js
@@ -0,0 +1,8 @@
+//document.querySelector("h1");
+$("h1");
+
+//document.querySelectorAll("button");
+$("button");
+
+//document.querySelector("h1").classList.add("big-title");
+$("h1").addClass("big-title");

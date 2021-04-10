2bb3b10a485beac6c1ec2e81ae3f3c760c688341
unknown <klinton.jp@gmail.com>
2021-04-09 21:51:31 -0600

    Added animation css based on event

--- The Complete 2021 Web Development Bootcamp/Javascript & DOM/Drum Kit/index.js
+++ The Complete 2021 Web Development Bootcamp/Javascript & DOM/Drum Kit/index.js
@@ -1,3 +1,5 @@
+//detecking screen clicks
+
 const numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
@@ -5,65 +7,78 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
 
         let buttonInnerHTML = this.innerHTML.substring(0,1);
 
-        switch (buttonInnerHTML) {
-            case "a":
-                const clap = new Audio("sounds/clap.wav");
-                console.log(buttonInnerHTML);
-                clap.play();
-            break;
+        makeSound(buttonInnerHTML);
 
-            case "s":
-                const hiHat = new Audio("sounds/hihat.wav");
-                hiHat.play();
-            break;
-
-            case "d":
-                const kick = new Audio("sounds/kick.wav");
-                kick.play();
-            break;
-
-            case "f":
-                const openHat = new Audio("sounds/openhat.wav");
-                openHat.play();
-            break;
-
-            case "g":
-                const boom = new Audio("sounds/boom.wav");
-                boom.play();
-            break;
-
-            case "h":
-                const ride = new Audio("sounds/ride.wav");
-                ride.play();
-            break;
-
-            case "j":
-                const snare = new Audio("sounds/snare.wav");
-                snare.play();
-            break;
-
-            case "k":
-                const tom = new Audio("sounds/tom.wav");
-                tom.play();
-            break;
-
-            case "l":
-                const tink = new Audio("sounds/tink.wav");
-                tink.play();
-            break;
+        buttonAnimation(buttonInnerHTML);
         
-            default: console.log();
-        }        
     });
 }
 
-/*
-<button class="a drum">a<span>clap</span></button>
-    <button class="s drum">s<span>hi hat</span></button>
-    <button class="d drum">d<span>kick</span></button>
-    <button class="f drum">f<span>open hat</span></button>
-    <button class="g drum">g<span>boom</span></button>
-    <button class="h drum">h<span>ride</span></button>
-    <button class="j drum">j<span>snare</span></button>
-    <button class="k drum">k<span>tom</span></button>
-    <button class="l drum">l<span>tink</span></button>*/
+//detecting button presses
+
+document.addEventListener("keydown", function(e) {
+    makeSound(e.key);
+
+    buttonAnimation(e.key);
+})
+
+function makeSound(key) {
+    switch (key) {
+        case "a":
+            const clap = new Audio("sounds/clap.wav");
+            clap.play();
+        break;
+
+        case "s":
+            const hiHat = new Audio("sounds/hihat.wav");
+            hiHat.play();
+        break;
+
+        case "d":
+            const kick = new Audio("sounds/kick.wav");
+            kick.play();
+        break;
+
+        case "f":
+            const openHat = new Audio("sounds/openhat.wav");
+            openHat.play();
+        break;
+
+        case "g":
+            const boom = new Audio("sounds/boom.wav");
+            boom.play();
+        break;
+
+        case "h":
+            const ride = new Audio("sounds/ride.wav");
+            ride.play();
+        break;
+
+        case "j":
+            const snare = new Audio("sounds/snare.wav");
+            snare.play();
+        break;
+
+        case "k":
+            const tom = new Audio("sounds/tom.wav");
+            tom.play();
+        break;
+
+        case "l":
+            const tink = new Audio("sounds/tink.wav");
+            tink.play();
+        break;
+    
+        default: console.log();
+    }
+}
+
+function buttonAnimation(currentKey) {
+
+    let activeButton = document.querySelector(`.${currentKey}`);
+    activeButton.classList.add("playing");
+    setTimeout(function() {
+        activeButton.classList.remove("playing");
+    }, 100);
+
+}

8670edeb8fdfd149403fa442d97f1f04c6ea004b
unknown <klinton.jp@gmail.com>
2021-04-10 17:56:07 -0600

    added various jQuery funtions for reference

--- The Complete 2021 Web Development Bootcamp/jQuery/index.js
+++ The Complete 2021 Web Development Bootcamp/jQuery/index.js
@@ -6,3 +6,40 @@ $("button");
 
 //document.querySelector("h1").classList.add("big-title");
 $("h1").addClass("big-title");
+
+//Text Context
+$("h1").text("Hello World");
+
+//innerHTML
+$("button").html("<em>Click me?</em>");
+
+//find out attribute
+console.log($("img").attr("src"));
+
+//add attribute, like href
+$("a").attr("href", "https://www.google.com");
+
+//add event listener
+$("h1").click(function() {
+    $("h1").css("color", "gray");
+})
+
+/*
+for (var i = 0; i<5; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function() {
+        document.querySelector("h1").style.color = "blue";
+    });
+}
+*/
+
+$("button").click(function() {
+    $("h1").css("color", "blue");
+});
+
+$("input").keydown(function(event) {
+    $("h1").text(event.key);
+})
+
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "red");
+})

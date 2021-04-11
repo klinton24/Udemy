702c572e7be9a696726d5344a319fe7b16d350d4
unknown <klinton.jp@gmail.com>
2021-04-11 10:30:16 -0600

    gamePattern and userClickPattern written

--- The Complete 2021 Web Development Bootcamp/jQuery/Simon Game/game.js
+++ The Complete 2021 Web Development Bootcamp/jQuery/Simon Game/game.js
@@ -1,10 +1,19 @@
-const gamePattern = [];
-
 const buttonColors = ["red", "blue", "green", "yellow"];
 
-function nextSequence () {
-    let number = console.log(Math.floor(Math.random() * 4));
-    let randomChosenColor =  buttonColors[number];
-    gamePattern.push(randomChosenColor);
+const gamePattern = [];
 
+const userClickedPattern = [];
+
+function nextSequence () {
+    let number = Math.floor(Math.random() * 4);
+    let randomChosenColor =  buttonColors[number];
+    $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    let audio = new Audio("sounds/"+randomChosenColor+".mp3");
+    audio.play();
+    gamePattern.push(randomChosenColor);
 };
+
+$(".btn").click(function() {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+})

384753797b179657196f8987dc9a2d48fdc2d36f
unknown <klinton.jp@gmail.com>
2021-04-11 11:58:48 -0600

    playSound, animatePress, game start functions

--- The Complete 2021 Web Development Bootcamp/jQuery/Simon Game/game.js
+++ The Complete 2021 Web Development Bootcamp/jQuery/Simon Game/game.js
@@ -4,16 +4,42 @@ const gamePattern = [];
 
 const userClickedPattern = [];
 
+let gameStarted = false;
+
+let level = 0
+
+function playSound(name) {
+    let audio = new Audio("sounds/"+name+".mp3");
+    audio.play();    
+}
+
+function animatePress(currentColor) {
+    $("#" +currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#" +currentColor).removeClass("pressed");
+    }, 100);
+}
+
 function nextSequence () {
     let number = Math.floor(Math.random() * 4);
     let randomChosenColor =  buttonColors[number];
     $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    let audio = new Audio("sounds/"+randomChosenColor+".mp3");
-    audio.play();
+    $("h1").text("Level " + level);
+    level++;
+    playSound(randomChosenColor);
     gamePattern.push(randomChosenColor);
 };
 
 $(".btn").click(function() {
     let userChosenColor = $(this).attr("id");
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
     userClickedPattern.push(userChosenColor);
 })
+
+$(document).keypress(function() {
+    if (gameStarted != true) {
+        gameStarted = true;
+        nextSequence();
+    }
+  });

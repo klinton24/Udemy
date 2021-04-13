16c7758f2579f5ba913f50dac875dc1436d9c0d8
unknown <klinton.jp@gmail.com>
2021-04-12 20:29:37 -0600

    fixed check answer, wrote start over function, fixed small errors

--- The Complete 2021 Web Development Bootcamp/jQuery/Simon Game/game.js
+++ The Complete 2021 Web Development Bootcamp/jQuery/Simon Game/game.js
@@ -1,12 +1,12 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 
-const gamePattern = [];
+let gamePattern = [];
 
 let userClickedPattern = [];
 
 let gameStarted = false;
 
-let level = 0
+let level = 0;
 
 function playSound(name) {
     let audio = new Audio("sounds/"+name+".mp3");
@@ -20,24 +20,37 @@ function animatePress(currentColor) {
     }, 100);
 }
 
-function checkAnswer (currentLevel) {
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    gameStarted = false;
+}
+
+function checkAnswer(currentLevel) {
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
         if (userClickedPattern.length === gamePattern.length){
         setTimeout(function () {
-            nextSequence()
+            nextSequence();
         }, 1000);
     }
-    } else { console.log("wrong");  
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
     };
 };
 
 function nextSequence () {
     userClickedPattern = [];
+    level++;
     let number = Math.floor(Math.random() * 4);
     let randomChosenColor =  buttonColors[number];
     $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     $("h1").text("Level " + level);
-    level++;
     playSound(randomChosenColor);
     gamePattern.push(randomChosenColor);
 };
@@ -47,7 +60,7 @@ $(".btn").click(function() {
     playSound(userChosenColor);
     animatePress(userChosenColor);
     userClickedPattern.push(userChosenColor);
-    checkAnswer();
+    checkAnswer(userClickedPattern.length-1);
 })
 
 $(document).keypress(function() {

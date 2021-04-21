e79acc5210d9b9264f84697d290a0b6f1aba7d07
unknown <klinton.jp@gmail.com>
2021-04-21 14:53:16 -0600

    added image and res.write

--- The Complete 2021 Web Development Bootcamp/Application Programming Interfaces/Weather Project/app.js
+++ The Complete 2021 Web Development Bootcamp/Application Programming Interfaces/Weather Project/app.js
@@ -3,11 +3,22 @@ const https = require("https");
 const app = express();
 
 app.get("/", function(req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=6a2a4b6f6e2431d3e1f2aa82c0c42957";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Osaka&appid=6a2a4b6f6e2431d3e1f2aa82c0c42957&units=imperial";
     https.get(url, function(response) {
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const weatherIcon = weatherData.weather[0].icon;
+            const weatherImage = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+            res.write("<h1>The temperature in Osaka, Japan is " + temp + "degrees.</h1>");
+            res.write("The weather is currently " + weatherDescription + ".</p>");
+            res.write("<img src=" + weatherImage +">");
+            res.send();
+        })
     })
-    res.send("Server is up and running.");
 });
 
 app.listen(3000, function() {

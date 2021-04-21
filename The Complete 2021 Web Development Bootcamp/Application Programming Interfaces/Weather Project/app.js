3be4eeaaa4a06e211c30c0f3b5e6147e7133ec10
unknown <klinton.jp@gmail.com>
2021-04-21 15:27:32 -0600

    Added user input

--- The Complete 2021 Web Development Bootcamp/Application Programming Interfaces/Weather Project/app.js
+++ The Complete 2021 Web Development Bootcamp/Application Programming Interfaces/Weather Project/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Osaka&appid=6a2a4b6f6e2431d3e1f2aa82c0c42957&units=imperial";
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    const query = req.body.cityName;
+    const apiKey = "6a2a4b6f6e2431d3e1f2aa82c0c42957";
+    const unit = "imperial"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`;
     https.get(url, function(response) {
         console.log(response.statusCode);
 
@@ -13,13 +23,14 @@ app.get("/", function(req, res) {
             const weatherDescription = weatherData.weather[0].description;
             const weatherIcon = weatherData.weather[0].icon;
             const weatherImage = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
-            res.write("<h1>The temperature in Osaka, Japan is " + temp + "degrees.</h1>");
+            res.write(`<h1>The temperature in ${query} is ${temp} degrees.</h1>`);
             res.write("The weather is currently " + weatherDescription + ".</p>");
             res.write("<img src=" + weatherImage +">");
             res.send();
         })
     })
-});
+
+})
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.")

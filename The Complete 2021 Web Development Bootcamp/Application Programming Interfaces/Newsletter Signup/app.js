a9989540f5f838adffc77ff7befc186e7bc4a7e4
unknown <klinton.jp@gmail.com>
2021-04-23 05:01:43 -0600

    sign up page pushes to mailchimp api

--- The Complete 2021 Web Development Bootcamp/Application Programming Interfaces/Newsletter Signup/app.js
+++ The Complete 2021 Web Development Bootcamp/Application Programming Interfaces/Newsletter Signup/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+require('dotenv').config();
 const app = express();
 
 app.use(express.static("public"));
@@ -11,10 +13,40 @@ app.get("/", function(req,res) {
 })
 
 app.post("/", function(req, res) {
-    let firstName = req.body.fName;
-    let lastName = req.body.lName;
-    let email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
     
+    const data = {
+        members: [
+            {
+                email_address:email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = process.env.URL+"/"+process.env.LIST_ID;
+
+    const options = {
+        method: "POST",
+        auth: "ksmith1:"+process.env.API_KEY
+    }
+
+    const request = https.request(url, options, function(response){
+        request.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
 });
 
 app.listen(3000, function(){

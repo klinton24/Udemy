2b7f4e42287d97cac1a268aa33497378d55d7b24
unknown <klinton.jp@gmail.com>
2021-04-25 15:00:41 -0600

    initial success and failure pages

--- The Complete 2021 Web Development Bootcamp/Application Programming Interfaces/Newsletter Signup/app.js
+++ The Complete 2021 Web Development Bootcamp/Application Programming Interfaces/Newsletter Signup/app.js
@@ -40,6 +40,14 @@ app.post("/", function(req, res) {
     }
 
     const request = https.request(url, options, function(response){
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+
         request.on("data", function(data){
             console.log(JSON.parse(data));
         })

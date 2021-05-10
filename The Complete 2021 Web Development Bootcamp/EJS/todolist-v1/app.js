92771a2cbfe5658520c7c8c3273ca00d81fa6df5
unknown <klinton.jp@gmail.com>
2021-05-10 08:45:39 -0600

    add toLocaleDateString

--- The Complete 2021 Web Development Bootcamp/EJS/todolist-v1/app.js
+++ The Complete 2021 Web Development Bootcamp/EJS/todolist-v1/app.js
@@ -1,42 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-	var today = new Date();
-	var currentDay = today.getDay();
-	var day = "";
+  let today = new Date();
 
-	switch (currentDay) {
-		case 0:
-			day = "Sunday";
-			break;
-		case 1:
-			day = "Monday";
-			break;
-		case 2:
-			day = "Tuesday";
-			break;
-		case 3:
-			day = "Wednesday";
-			break;
-		case 4:
-			day = "Thursday";
-			break;
-		case 5:
-			day = "Friday";
-			break;
-		case 6:
-			day = "Saturday";
-            break;
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-		default:
-        console.log("Error: current day is equal to: " + currentDay);
-	}
+  let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {
-      kindOfDay: day });
+  res.render("list", {
+    kindOfDay: day,
+  });
 });
 
-app.listen(8080, function () {
-	console.log("Server started on port 8080");
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
 });

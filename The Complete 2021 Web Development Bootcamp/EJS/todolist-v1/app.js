88a6633221feef7606b4cc8ec4f9a2f73b85b554
unknown <klinton.jp@gmail.com>
2021-05-11 09:20:16 -0600

    Add EJS for loop for list items

--- The Complete 2021 Web Development Bootcamp/EJS/todolist-v1/app.js
+++ The Complete 2021 Web Development Bootcamp/EJS/todolist-v1/app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+let items = [];
 
 app.get("/", function (req, res) {
   let today = new Date();
@@ -17,8 +22,14 @@ app.get("/", function (req, res) {
 
   res.render("list", {
     kindOfDay: day,
+    newListItems: items
   });
 });
+
+app.post("/", function(req,res) {
+  items.push(req.body.newItem);
+  res.redirect("/")
+})
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");

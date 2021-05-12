5120ecccb163a424a29482d8c3e34b1ed7a84a0f
unknown <klinton.jp@gmail.com>
2021-05-12 17:44:00 -0600

    add new work route for different template

--- The Complete 2021 Web Development Bootcamp/EJS/todolist-v1/app.js
+++ The Complete 2021 Web Development Bootcamp/EJS/todolist-v1/app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 let items = [];
+let workItems = [];
 
 app.get("/", function (req, res) {
   let today = new Date();
@@ -27,10 +28,20 @@ app.get("/", function (req, res) {
   });
 });
 
+app.get("/work", function (req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
 app.post("/", function(req,res) {
-  items.push(req.body.newItem);
-  res.redirect("/")
-})
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/")
+  }
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");

e8097980d29df8f084d4aff3ded22a779a28d912
unknown <klinton.jp@gmail.com>
2021-05-14 15:14:24 -0600

    create compose ejs

--- The Complete 2021 Web Development Bootcamp/EJS/blog-site/app.js
+++ The Complete 2021 Web Development Bootcamp/EJS/blog-site/app.js
@@ -31,6 +31,15 @@ app.get("/contact", (req, res) => {
   });
 });
 
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req,res) => {
+  let entry = req.body.newEntry;
+  console.log(entry);
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
